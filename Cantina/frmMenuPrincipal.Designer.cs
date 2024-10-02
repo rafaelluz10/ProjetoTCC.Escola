0ff0fbf8ec52462b0280b8fe44ecc616edd30a03
rafaelluz10 <rafael.luztp1@gmail.com>
2024-10-01 22:08:33 -0300

    teste

--- Cantina/frmMenuPrincipal.Designer.cs
+++ Cantina/frmMenuPrincipal.Designer.cs
@@ -44,7 +44,7 @@ namespace Cantina
             this.btnVoltar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnVoltar.ForeColor = System.Drawing.SystemColors.Control;
             this.btnVoltar.Image = ((System.Drawing.Image)(resources.GetObject("btnVoltar.Image")));
-            this.btnVoltar.Location = new System.Drawing.Point(770, 516);
+            this.btnVoltar.Location = new System.Drawing.Point(859, 548);
             this.btnVoltar.Name = "btnVoltar";
             this.btnVoltar.Size = new System.Drawing.Size(150, 51);
             this.btnVoltar.TabIndex = 0;
@@ -60,11 +60,11 @@ namespace Cantina
             this.btnFuncionarios.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnFuncionarios.ForeColor = System.Drawing.SystemColors.Control;
             this.btnFuncionarios.Image = ((System.Drawing.Image)(resources.GetObject("btnFuncionarios.Image")));
-            this.btnFuncionarios.Location = new System.Drawing.Point(34, 59);
+            this.btnFuncionarios.Location = new System.Drawing.Point(62, 59);
             this.btnFuncionarios.Name = "btnFuncionarios";
             this.btnFuncionarios.Size = new System.Drawing.Size(249, 219);
             this.btnFuncionarios.TabIndex = 1;
-            this.btnFuncionarios.Text = "&Alunos";
+            this.btnFuncionarios.Text = "&Cadastro de Funcionários";
             this.btnFuncionarios.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btnFuncionarios.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
             this.btnFuncionarios.UseVisualStyleBackColor = true;
@@ -76,11 +76,11 @@ namespace Cantina
             this.btnClientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnClientes.ForeColor = System.Drawing.SystemColors.Control;
             this.btnClientes.Image = ((System.Drawing.Image)(resources.GetObject("btnClientes.Image")));
-            this.btnClientes.Location = new System.Drawing.Point(354, 59);
+            this.btnClientes.Location = new System.Drawing.Point(375, 59);
             this.btnClientes.Name = "btnClientes";
             this.btnClientes.Size = new System.Drawing.Size(260, 219);
             this.btnClientes.TabIndex = 2;
-            this.btnClientes.Text = "&Funcionários";
+            this.btnClientes.Text = "&Alunos";
             this.btnClientes.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btnClientes.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
             this.btnClientes.UseVisualStyleBackColor = true;
@@ -91,7 +91,7 @@ namespace Cantina
             this.btnProdutos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnProdutos.ForeColor = System.Drawing.SystemColors.Control;
             this.btnProdutos.Image = ((System.Drawing.Image)(resources.GetObject("btnProdutos.Image")));
-            this.btnProdutos.Location = new System.Drawing.Point(687, 206);
+            this.btnProdutos.Location = new System.Drawing.Point(714, 205);
             this.btnProdutos.Name = "btnProdutos";
             this.btnProdutos.Size = new System.Drawing.Size(265, 215);
             this.btnProdutos.TabIndex = 3;
@@ -106,7 +106,7 @@ namespace Cantina
             this.btnVendas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnVendas.ForeColor = System.Drawing.SystemColors.Control;
             this.btnVendas.Image = ((System.Drawing.Image)(resources.GetObject("btnVendas.Image")));
-            this.btnVendas.Location = new System.Drawing.Point(354, 340);
+            this.btnVendas.Location = new System.Drawing.Point(375, 340);
             this.btnVendas.Name = "btnVendas";
             this.btnVendas.Size = new System.Drawing.Size(260, 216);
             this.btnVendas.TabIndex = 6;
@@ -121,7 +121,7 @@ namespace Cantina
             this.btnUsuarios.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnUsuarios.ForeColor = System.Drawing.SystemColors.Control;
             this.btnUsuarios.Image = ((System.Drawing.Image)(resources.GetObject("btnUsuarios.Image")));
-            this.btnUsuarios.Location = new System.Drawing.Point(34, 340);
+            this.btnUsuarios.Location = new System.Drawing.Point(62, 340);
             this.btnUsuarios.Name = "btnUsuarios";
             this.btnUsuarios.Size = new System.Drawing.Size(249, 216);
             this.btnUsuarios.TabIndex = 5;
@@ -135,7 +135,7 @@ namespace Cantina
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(1014, 620);
+            this.ClientSize = new System.Drawing.Size(1092, 642);
             this.Controls.Add(this.btnVendas);
             this.Controls.Add(this.btnUsuarios);
             this.Controls.Add(this.btnProdutos);
